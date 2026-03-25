06d49c9dd3457ee3854edfc4202047f0e5fe02d1
Ryan-Mehr <ryanmehr@my.yorku.ca>
2026-03-24 20:11:22 -0400

    fixed db issue.

--- src/main/java/Repository/DatabaseManager.java
+++ src/main/java/Repository/DatabaseManager.java
@@ -8,33 +8,33 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-//import GlobalVariables.ApplicationConstants;
+import GlobalVariables.ApplicationConstants;
 //
 //import java.sql.*;
 
 public class DatabaseManager {
-    private static Properties props = new Properties();
-
-    static {
-        try {
-            props.load(new FileInputStream("config.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static Connection getConnection() throws SQLException {
-        String url = props.getProperty("db_link");
-        String user = props.getProperty("db_user");
-        String password = props.getProperty("db_password");
-
-        return DriverManager.getConnection(url, user, password);
-    }
-//    public static Connection getConnection() throws SQLException {
-//        return DriverManager.getConnection(
-//                ApplicationConstants.db_link,
-//                ApplicationConstants.db_user,
-//                ApplicationConstants.db_password
-//        );
+//    private static Properties props = new Properties();
+//
+//    static {
+//        try {
+//            props.load(new FileInputStream("config.properties"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 //    }
+//
+//    public static Connection getConnection() throws SQLException {
+//        String url = props.getProperty("db_link");
+//        String user = props.getProperty("db_user");
+//        String password = props.getProperty("db_password");
+//
+//        return DriverManager.getConnection(url, user, password);
+//    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                ApplicationConstants.db_link,
+                ApplicationConstants.db_user,
+                ApplicationConstants.db_password
+        );
+    }
 }
